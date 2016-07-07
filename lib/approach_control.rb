1d90cb8301a096f17e5f3f020baae447c581d8f2
Matt Towers <matt@realself.com>
2016-07-07 09:09:48 -0700

    * tweak comment message * refresh credentails after expiration * add comments to gemspec * update README

--- lib/approach_control.rb
+++ lib/approach_control.rb
@@ -2,12 +2,12 @@ require "approach_control/version"
 require "redd"
 
 module ApproachControl
-  TITLE_SLUG_REGEX = /^([A-Z0-9]{3,4}|\([A-Z0-9]{3,4}\))+(?:[,\s-])+(?:RWY|Runway|\s)*(\d{1,2}[RLC]{0,1})*/
+  TITLE_SLUG_REGEX  = /^([A-Z0-9]{3,4}|\([A-Z0-9]{3,4}\))+(?:[,\s-])+(?:RWY|Runway|\s)*(\d{1,2}[RLC]{0,1})*/
   ICAO_REGEX        = /\(([A-Z0-9]{3,4})\)+/
   RWY_REGEX         = /(?:(?:RWY|Rwy|rwy|Runway|runway)+(?:\s)*([0-9]{1,2}(?:[RLC]{1})*))/
 
   def self.auth!
-    r = Redd.it(
+    @reddit = Redd.it(
       :script,
       ENV['CLIENT_ID'],
       ENV['CLIENT_SECRET'],
@@ -15,12 +15,13 @@ module ApproachControl
       ENV['REDDIT_PASSWORD'],
       user_agent: "Approach Control Bot v0.0.1")
 
-    r.authorize!
-    r
+    @reddit.authorize!
   end
 
-  def self.stream_all! reddit
-    reddit.stream :get_new, 'shortfinal' do |item|
+  def self.stream_all!
+    @reddit.stream :get_new, 'shortfinal' do |item|
+      @reddit.refresh_access! if @reddit.access.expired?
+
       puts item.title
       title_match = item.title.match(TITLE_SLUG_REGEX)
       icao_match  = item.title.match(ICAO_REGEX)
@@ -41,13 +42,8 @@ module ApproachControl
         end
 
         unless commentators.include? 'approach_control'
-          comment  = "\nAirport:  [#{icao}](http://ourairports.com/airports/#{icao}/pilot-info.html#general)"
-          comment += "\n&nbsp;"
-          comment += "\n\nRunway:   #{rwy}" if rwy
-          comment += "\n&nbsp;"
-          comment += "\n\n_posted by approach_control bot_"
-          comment += "\n&nbsp;"
-          comment += "\n\n_if this data is incorrect please click 'report' to notifiy the moderators_"
+          comment  = "* Airport:  [#{icao}](http://ourairports.com/airports/#{icao}/pilot-info.html#general)"
+          comment += "\n* Runway:   #{rwy}" if rwy
 
           item.add_comment comment
         end
@@ -56,16 +52,28 @@ module ApproachControl
   end
 end
 
-reddit = ApproachControl.auth!
 
 begin
-  ApproachControl.stream_all! reddit
+  ApproachControl.auth!
+  ApproachControl.stream_all!
 rescue Redd::Error::RateLimited => error
   sleep(error.time)
   retry
+rescue Redd::Error::InvalidOAuth2Credentials
+  puts "Expired OAuth Token.  Reauthorizing after a little nap..."
+  sleep(60)
+  ApproachControl.auth!
+  retry
 rescue Redd::Error => error
+  puts error.message
+  puts error.backtrace.join("\n")
   # 5-something errors are usually errors on reddit's end.
   raise error unless (500...600).include?(error.code)
+  retry
+rescue Net::OpenTimeout => ot
+  puts ot.message
+  sleep(60)
+  ApproachControl.auth!
   retry
 end
 
